d90532376a696e01e317e29a1d28c160b7f28467
Julian <miramontoyajulian@gmail.com>
2023-07-07 18:56:31 -0500

    Agregado el controlador de login, registro y recuperacion de contraseña

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     function showLoginView() {
-        return view("login");
+        
     }
 
     function showRestoreView(){
--- app/Http/Controllers/RestoreController.php
+++ app/Http/Controllers/RestoreController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class User extends Controller
+class RestoreController extends Controller
 {
-    
+    //
 }
